f298cd7e34c75a47ba853055140cac2b9b84329c
Kate <kate@25sprout.com>
2023-04-03 14:46:25 +0800

    feat: add image scrollTrigger effect

--- src/components/molecules/WorkItem/index.tsx
+++ src/components/molecules/WorkItem/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 import WorksHeader from "../../atoms/WorksHeader";
 
@@ -7,6 +10,55 @@ import "./style.scss";
 
 export default function WorkItem() {
   const { t } = useTranslation(["works"]);
+
+  const image01 = useRef<HTMLDivElement>(null);
+  const image02 = useRef<HTMLDivElement>(null);
+  const image03 = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(image01.current, {
+        scrollTrigger: {
+          trigger: image01.current,
+          start: "top 700px",
+          toggleClass: "active",
+          // markers: {
+          //   startColor: "purple",
+          //   endColor: "fuchsia",
+          //   fontSize: "3rem",
+          // }
+        },
+      });
+      gsap.to(image02.current, {
+        scrollTrigger: {
+          trigger: image02.current,
+          start: "top 600px",
+          toggleClass: "active",
+          // markers: {
+          //   startColor: "purple",
+          //   endColor: "fuchsia",
+          //   fontSize: "3rem",
+          // }
+        },
+      });
+      gsap.to(image03.current, {
+        scrollTrigger: {
+          trigger: image03.current,
+          start: "top 550px",
+          toggleClass: "active",
+          // markers: {
+          //   startColor: "purple",
+          //   endColor: "fuchsia",
+          //   fontSize: "3rem",
+          // }
+        },
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  });
   return (
     <div className="workItem-wrapper">
       <div className="workItem">
@@ -20,21 +72,21 @@ export default function WorkItem() {
             <a href="#" className="r-and-b">
               <div className="imgbox">
                 <div className="image-inner">
-                  <div className="image"></div>
+                  <div className="image" ref={image01}></div>
                 </div>
               </div>
             </a>
             <a href="#" className="rap">
               <div className="imgbox">
                 <div className="image-inner">
-                  <div className="image"></div>
+                  <div className="image" ref={image02}></div>
                 </div>
               </div>
             </a>
             <a href="#" className="kpop">
               <div className="imgbox">
                 <div className="image-inner">
-                  <div className="image"></div>
+                  <div className="image" ref={image03}></div>
                 </div>
               </div>
             </a>
--- src/components/molecules/WorkItemEnterprise/index.tsx
+++ src/components/molecules/WorkItemEnterprise/index.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 import WorksHeader from "../../atoms/WorksHeader";
 
@@ -8,6 +11,42 @@ import "./style.scss";
 
 export default function WorkItemEnterprise() {
   const { t } = useTranslation(["works"]);
+
+  const image01 = useRef<HTMLDivElement>(null);
+  const image02 = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(image01.current, {
+        scrollTrigger: {
+          trigger: image01.current,
+          start: "top 700px",
+          toggleClass: "active",
+          // markers: {
+          //   startColor: "purple",
+          //   endColor: "fuchsia",
+          //   fontSize: "3rem",
+          // }
+        },
+      });
+      gsap.to(image02.current, {
+        scrollTrigger: {
+          trigger: image02.current,
+          start: "top 600px",
+          toggleClass: "active",
+          // markers: {
+          //   startColor: "purple",
+          //   endColor: "fuchsia",
+          //   fontSize: "3rem",
+          // }
+        },
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  });
   return (
     <div className="workItem-wrapper">
       <div className="workItem">
@@ -21,14 +60,14 @@ export default function WorkItemEnterprise() {
             <a href="#" className="banner">
               <div className="imgbox">
                 <div className="image-inner">
-                  <div className="image"></div>
+                  <div className="image" ref={image01}></div>
                 </div>
               </div>
             </a>
             <a href="#" className="comment">
               <div className="imgbox">
                 <div className="image-inner">
-                  <div className="image"></div>
+                  <div className="image" ref={image02}></div>
                 </div>
               </div>
             </a>
