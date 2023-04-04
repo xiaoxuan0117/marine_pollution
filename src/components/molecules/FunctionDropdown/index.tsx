c5cb27b226b59030ded26b2c0f8a4855863a7c82
xiaoxuan0117 <xiaoxuanpan0117@gmail.com>
2023-04-05 03:58:33 +0800

    feat: add resume pdf link

--- src/components/molecules/FunctionDropdown/index.tsx
+++ src/components/molecules/FunctionDropdown/index.tsx
@@ -59,8 +59,8 @@ export default function FunctionDropdown(props: {
         "zh option",
         "en option",
         "scroll-top",
+        "resume scroll-top",
       ];
-      console.log(isOpen);
       if (isOpen && !preventList.includes(target.classList.value)) {
         api.start({
           from: {
@@ -115,6 +115,12 @@ export default function FunctionDropdown(props: {
                 </div>
               </div>
             </div>
+            <a
+              className="resume scroll-top"
+              href="https://drive.google.com/file/d/1lELRNel5cNHN1nys2IewBKjQY-ffA_VQ/view?usp=share_link"
+            >
+              {t("download-resume")}
+            </a>
           </div>
         </animated.div>
       </div>
